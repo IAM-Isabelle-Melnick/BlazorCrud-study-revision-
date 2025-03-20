35199e42470e1d1fdf39bd9a55e857d6664ff539
IAM Isabelle <849879+IAM-Isabelle-Melnick@users.noreply.github.com>
2025-03-20 14:59:08 -0400

    First commit

---
+++ SimpleBookCatalog/SimpleBookCatalog.Infrastructure/Context/SimpleBookCatalogDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleBookCatalog.Domain.Entities;
+
+namespace SimpleBookCatalog.Infrastructure.Context
+{
+    public class SimpleBookCatalogDbContext : DbContext
+    {
+        public SimpleBookCatalogDbContext(DbContextOptions<SimpleBookCatalogDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+        // if we do not want to use data annotations in entities, we can use this (fluent api way of things)
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<Book>().Property(e => e.Title).IsRequired().HasMaxLength(100);
+        //}
+    }
+}
